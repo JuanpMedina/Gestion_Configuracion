e853e52b2a98bdfb924bf2e0a6560fcd34704adb
Juan Pablo Medina Diaz <juampamedinadiaz2002@gmail.com>
2022-04-21 12:51:24 -0500

    Correccio errores 2

--- myapp.py
+++ myapp.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.get("/hi")
 def say_hi():
-  """Funcion para retornar"""
-  return 'Hola soy Alexis'
+    """Funcion para retornar"""
+    return 'Hola soy Alexis'
 
 if __name__=='__main__':
-  uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
